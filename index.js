c5ebbf44498041b1d46d6902df350a5b8fee5cff
tal6203 <tal6203@gmail.com>
2022-11-28 18:50:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const os = require('os');
 console.log(`Free memory: ${os.freemem()}`);
 console.log(`Total memorey: ${os.totalmem()}`);
+
+const fs = require(`fs`);
+const file = fs.readdirSync(`./`);
+console.log(`[sync] ${file}`);
+
+
+fs.readdir('./',(err,file) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(`[async]Free memory ${file}`);
+    }
+
+});
+
+console.log("between sync to async");
