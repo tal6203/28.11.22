e799c6dd38ac8a96d9d0b039ec52c8fc6bbf741f
tal6203 <tal6203@gmail.com>
2022-11-28 21:24:12 +0200

    update

--- indexdb.js
+++ indexdb.js
@@ -11,3 +11,60 @@ db.serialize(() => {
         }
     });
 });
+
+// const user = [6, "Moshe", 29, "USA", 70000];
+
+// function insert(db,user,err){
+// const insert_user = `INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+//                      VALUES(?, ?, ?, ?, ?);`
+// db.run(insert_user, user, (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log(user);
+//     }
+// });
+// }
+function up(db, id, new_salary) {
+    const sql_update = `UPDATE COMPANY
+                        SET SALARY = ?
+                        WHERE id = ? ;`;
+    db.run(sql_update, [new_salary, id], err => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(new_salary);
+        }
+    });
+
+}
+
+function delete_by_id(db, id) {
+    const sql_delete = `DELETE FROM COMPANY WHERE ID = ?;`
+    db.run(sql_delete, [id], err => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(`id ${id} delete`);
+        }
+    });
+}
+
+
+
+setTimeout(() => up(db, 6, 45000), 1000);
+
+setTimeout(() => delete_by_id(db, 6), 1000);
+
+
+// db.close((err) => {
+//     if (err) {
+//         console.log(err.message);
+//     }
+//     else {
+//         console.log("Database connection close!");
+//     }
+// })
